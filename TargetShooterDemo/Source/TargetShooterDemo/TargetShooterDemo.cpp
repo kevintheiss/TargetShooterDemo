86d3a1a7499e8738b751a90a49ba81620c1f2ce4
kevintheiss <ktheiss1990@gmail.com>
2024-03-11 10:56:26 -0700

    Initial commit

---
+++ TargetShooterDemo/Source/TargetShooterDemo/TargetShooterDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TargetShooterDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TargetShooterDemo, "TargetShooterDemo" );
---
+++ TargetShooterDemo/Source/TargetShooterDemo/TargetShooterDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TargetShooterDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TARGETSHOOTERDEMO_API ATargetShooterDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
